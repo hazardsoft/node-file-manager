ce91a220e51eedd47a5738f4f53b0f0035c28abd
Henadzi Shutko <hazardsoft@gmail.com>
2025-05-06 01:07:18 +0300

    feat: start application at homedir

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import { readArg } from "./args.js"
 import * as readline from 'node:readline/promises';
-import { stdin, stdout } from 'node:process';
+import { stdin, stdout, cwd, chdir } from 'node:process';
+import {homedir} from "node:os"
 
 const printCurrentWorkingDir = () => {
-    console.log(`You are currently in ${process.cwd()}`)
+    console.log(`You are currently in ${cwd()}`)
 }
 
 const username = readArg('username')
 console.log(`Welcome to the File Manager, ${username}!`)
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
+chdir(homedir())
 printCurrentWorkingDir()
 
 rl.on('line', async (line) => {
